171f66f2f7add04443a7a5e79a02df071d2c5e07
hongkedavid <hongkedavid@gmail.com>
2012-06-02 13:50:19 +0000

    FBLogin.java committed

---
+++ FBLogin.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import javax.swing.text.html.HTML;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomNodeList;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
+import com.gargoylesoftware.htmlunit.html.HtmlResetInput;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+
+public class FBLogin
+{
+	private static String file_name = "page2";
+	
+	public static void readHtmlPage() {
+		
+		  try {
+		       FileInputStream fstream = new FileInputStream(file_name);
+		       DataInputStream in = new DataInputStream(fstream);
+		       BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		       String strLine;
+		  
+		       while ((strLine = br.readLine()) != null) {
+		              strLine.contains("https://")
+		       }
+		  
+		       in.close();
+		  } 
+		  catch (Exception e) {
+		       System.err.println("Error: " + e.getMessage());
+		  }
+		  
+	}
+	
+	public static void main(String[] args) throws Exception {
+	  	
+		WebClient wc = new WebClient();
+        HtmlPage page = (HtmlPage) wc.getPage("http://www.facebook.com/hongkedavid");
+        List<HtmlForm> form_list = page.getForms();
+        System.out.println(form_list.size() + " forms in this page"); 
+        
+        HtmlForm form = form_list.get(0);//page.getFormByName("login_form");
+        HtmlTextInput email = (HtmlTextInput) form.getInputByName("email");
+        email.setText("hongkedavid@gmail.com");
+        HtmlPasswordInput passwd = (HtmlPasswordInput) form.getInputByName("pass");
+        passwd.setText("88927Hk");
+        
+        HtmlSubmitInput button = (HtmlSubmitInput) form.getInputByValue("Log In");
+        HtmlPage page2 = (HtmlPage) button.click();
+        
+        form_list = page2.getForms();
+        System.out.println(form_list.size() + " forms in this page");
+        //DomNodeList<HtmlElement> img_list = page2.getElementsByTagName("img");
+        //System.out.println(img_list.size() + " images in this page");
+        //for (int i = 0; i < img_list.size(); i++)
+        //	System.out.println(img_list.get(i).getId());
+        File page2_fd = new File(file_name);
+        page2.save(page2_fd);
+        System.out.println(page2_fd.length() + " bytes");
+        readHtmlPage();
+        
+    }
+}
